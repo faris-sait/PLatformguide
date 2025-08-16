fa20d514d5327d9c829a77c1e1e859b6409fd5f7
Mohammed Faris Sait <farissait@gmail.com>
2025-08-16 15:46:54 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -412,4 +412,4 @@ async def get_cheapest_by_category():
     return dict(cheapest_by_category)
 
 if __name__ == "__main__":
-    uvicorn.run("server:app", host="0.0.0.0", port=8001, reload=True)
+    uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
